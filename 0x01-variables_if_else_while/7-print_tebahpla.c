ec9b5ff488f51aa0aded7f9568ef903f0840dee6
osahon <rillcod@gmail.com>
2023-06-19 06:00:21 +0100

    all

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print lower aphlabet backward
+ * Return: 0
+ */
+int main(void)
+{ 
+char c;
+
+for (c='z'; c>='a'; --c)
+putchar (c)
+
+putchar('\n');
+
+Return (0);
+}
